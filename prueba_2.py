4f8e7adde895f6bee70f60565b6da34e768d8d2c
Rafael-2806 <113630636+Rafael-2806@users.noreply.github.com>
2023-10-30 13:43:40 +0100

    update

--- prueba_2.py
+++ prueba_2.py
@@ -1,16 +1,19 @@
 
 
 
-
 def quicksort(lista):
+    
     if len(lista)<=1:
-        return lista 
-    else: 
-        pivote = lista[0]
-        menores= [x for x in lista[1:]if x <= pivote]
-        mayores = [x for x in lista[1:]if x > pivote]        
+        return lista
+    else:
+        pivote= lista[0]
+        
+        menores= [x for x in lista[1:] if x <=pivote]
+        mayores= [x for x in lista[1:] if x >pivote]
         return quicksort(menores)+ [pivote]+ quicksort(mayores)
+
+# Prueba
 lista = [64, 34, 25, 12, 22, 11, 90]
 print("Lista original:", lista)
-
-print("Lista ordenada:", quicksort(lista))
+lista_ordenada = quicksort(lista)
+print("Lista ordenada:", lista_ordenada)
